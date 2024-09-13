9577b9facaa5c705d52f776b3891e17f822a2f22
rgcbdigital <cheysser@gmail.com>
2024-09-13 10:44:50 +0800

    updated  with array of person info

--- main.rb
+++ main.rb
@@ -1,29 +1,32 @@
 #```ruby
 # ```
-
-person = {
-  person1: { national_id: 1, name: "Edward", age: 18 },
-  person2: { national_id: 2, name: "Mark", age: 19 },
-  person3: { national_id: 3, name: "Clair", age: 20 },
-  person4: { national_id: 4, name: "Czed", age: 18 },
-  person5: { national_id: 5, name: "Rose", age: 32 },
-  person6: { national_id: 6, name: "Cheysser", age: 32 },
-  person7: { national_id: 7, name: "Lance", age: 11 },
-  person8: { national_id: 8, name: "Usagi", age: 18 },
-  person9: { national_id: 9, name: "Fan", age: 18 },
-  person10: { national_id: 10, name: "Liu", age: 18 },
-  person11: { national_id: 11, name: "Long", age: 18 },
-  person12: { national_id: 12, name: "Kang", age: 30 },
-  person13: { national_id: 13, name: "Gina", age: 75 },
-  person14: { national_id: 14, name: "Karen", age: 60 },
-  person15: { national_id: 15, name: "Claudine", age: 80 },
-  person16: { national_id: 16, name: "Jeff", age: 90 },
-  person17: { national_id: 17, name: "Zain", age: 18 },
-  person18: { national_id: 18, name: "Loi", age: 15 },
-  person19: { national_id: 19, name: "Kath", age: 18 },
-  person20: { national_id: 20, name: "Sid", age: 21 }
-
+person = [
+  { national_id: 1, name: "Edward", age: 18 },
+  { national_id: 2, name: "Mark", age: 19 },
+  { national_id: 3, name: "Clair", age: 20 },
+  { national_id: 4, name: "Czed", age: 18 },
+  { national_id: 5, name: "Rose", age: 32 },
+  { national_id: 6, name: "Cheysser", age: 32 },
+  { national_id: 7, name: "Lance", age: 11 },
+  { national_id: 8, name: "Usagi", age: 18 },
+  { national_id: 9, name: "Fan", age: 18 },
+  { national_id: 10, name: "Liu", age: 18 },
+  { national_id: 11, name: "Long", age: 18 },
+  { national_id: 12, name: "Kang", age: 30 },
+  {national_id: 13, name: "Gina", age: 75 },
+  { national_id: 14, name: "Karen", age: 60 },
+  { national_id: 15, name: "Claudine", age: 80 },
+  { national_id: 16, name: "Jeff", age: 90 },
+  { national_id: 17, name: "Zain", age: 18 },
+  { national_id: 18, name: "Loi", age: 15 },
+  { national_id: 19, name: "Kath", age: 18 },
+  { national_id: 20, name: "Sid", age: 21 }
 }
+]
 
 print person
+
+
+
+
 
