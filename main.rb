b50591be75e9c348dfdeaa4d935f71437d8f19b3
rgcbdigital <cheysser@gmail.com>
2024-09-13 12:34:26 +0800

    checks if user already in list and adds if not

--- main.rb
+++ main.rb
@@ -13,7 +13,7 @@ person = [
   { national_id: 10, name: "Liu", age: 18 },
   { national_id: 11, name: "Long", age: 18 },
   { national_id: 12, name: "Kang", age: 30 },
-  {national_id: 13, name: "Gina", age: 75 },
+  { national_id: 13, name: "Gina", age: 75 },
   { national_id: 14, name: "Karen", age: 60 },
   { national_id: 15, name: "Claudine", age: 80 },
   { national_id: 16, name: "Jeff", age: 90 },
@@ -21,10 +21,32 @@ person = [
   { national_id: 18, name: "Loi", age: 15 },
   { national_id: 19, name: "Kath", age: 18 },
   { national_id: 20, name: "Sid", age: 21 }
-}
+
 ]
 
-print person
+#print person
+
+puts "What is your national id?"
+national_id = gets.chomp.to_i
+
+if person.any? {|h| h[:national_id] == national_id}
+  puts "Failed to add: National ID already exists."
+else
+    puts "What is your name?"
+end
+
+name = gets.chomp.to_s
+puts "What is your age?"
+age = gets.chomp.to_i
+
+
+new_user = { national_id: national_id, name: name  , age: age }
+person.insert(0,new_user)
+
+puts "User added successfully!"
+
+puts person
+
 
 
 
